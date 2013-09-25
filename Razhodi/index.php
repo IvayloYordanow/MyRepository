adc71a449f198536a5e6780c45ac5f68af359386
IvayloYordanow <i.yordanow@gmail.com>
2013-09-25 12:42:28 +0300

    Update index.php

--- Razhodi/index.php
+++ Razhodi/index.php
@@ -1,88 +1,105 @@
-<?php
-$pageTitle='Списък';
-include 'includes/header.php';
+<?PHP
+$pagetitle='Expenses Table';
+include 'header.php';
+require 'Array.php';
+
+    
 ?>
-<a href="form.php">Добави нов разход</a>
-
 <form method="POST">
-	<div>
-    <span>Вид:
-        <select name="type">			
-			<?php 
-			/* да се показва избраната стойност, а не тази по подразбиране */
-			if(!$_POST['type']||$_POST['type'] == 'all') { 	
-				echo '<option value="all" selected >Всички</option>'; 				
-				foreach ($type as $key=>$type_value) {						
-					echo '<option value="'.$key.'">' . $type_value . '</option>';
-				}
-			} 
-			else {				
-				echo '<option value="all" >Всички</option>';									
-				foreach ($type as $key=>$type_value) {
-					if ($_POST['type'] == $key) {$selected = 'selected';}
-					else {$selected = '';}
-					echo '<option value="'.$key.'"'.$selected.'>' . $type_value . '</option>';
-				}
-			}
-            ?>		
-        </select> 
+        
+           <div>
+           <select name="groups">
+               <?php
+               
+               if(isset ($_POST["groups"])){
+                   $groups=$_POST["groups"];
+                   
+               }
+               
+               else {
+                   
+                  $groups=''; 
+                   
+               }
+               
+               foreach ($Type as $key=> $value)
+                   {
+                   if($key ==$groups ){
+                       $selected='selected';
+                   }
+                   else {
+                       $selected='';
+                   }
+                   
+                   
+                   
+                  echo '<option '. $selected. ' value= "'.$key.'">'.$value.'</option>';
+               }
+               
+               ?> 
+           </select>
+               <input type="submit" value="Filter"/>
+            
+          </div>
+    </form>
+    <br>
+   
 
-    </span>  
-	<span><input type="submit" value="Филтрирай" /></span>
-    </div>
-</form>
+        <a href="form.php">Expenses</a>
 
-<table border="1">
-    <tr>
-		<td>Дата</td>	
-        <td>Име</td>
-        <td>Сума</td>
-        <td>Вид</td>
-    </tr>
-    <?php
-	$sum=0;
-    if(file_exists('data.txt')){
-        $result=  file('data.txt');
-		
-        foreach ($result as $value) {						
-            $columns=  explode('!', $value);  	
-			
-			if($_POST){				
-				$selected_type=trim($_POST['type']);	
-				/* да се показват само разходите за избрания филтър и тяхната сума */
-				if((int)$columns[3] == $selected_type || $selected_type == 'all'){			
-					echo '<tr>
-						<td>'.$columns[0].'</td>
-						<td>'.$columns[1].'</td>
-						<td>'.$columns[2].'</td>
-						<td>'.$type[trim($columns[3])].'</td>
-						</tr>';
-					$sum+= (float)$columns[2];
-				}					
-			}
-			/* при първоначално зареждане на страницата да се показват всички разходи и тяхната сума */
-			else {
-				echo '<tr>
-					<td>'.$columns[0].'</td>
-					<td>'.$columns[1].'</td>
-					<td>'.$columns[2].'</td>
-					<td>'.$type[trim($columns[3])].'</td>
-					</tr>';		
-				$sum+= (float)$columns[2];
-			}					
-        }
-		$sum = round($sum, 2);
-    } 		
-    ?>
-		
-	<tr>
-		<td> -- </td>	
-		<td> -- </td>
-		<td><?php echo $sum ?></td>
-		<td> -- </td>
-	</tr>      
-</table>
+        <table border="1">
+            <tr>
+                <td>Date</td>
+                <td>Name</td>
+                <td>Expenses</td>
+                <td>Group</td> 
+            </tr>
+            
+            
+            <?php
+            $sum=0;
+            if(file_exists('data.txt')){
+                
+                $result=  file('data.txt');
+             
+                foreach ($result as $value) {
+                                 $columns=  explode('!', $value);
+                
+                 
+                                 if (isset($_POST['groups'])) {
+                                             $group = $_POST['groups'];
+                                             
+                                 }
+                                   else {
+                                       $group=-1;
+                                   }
+                                   
+                                  if ($group ==-1||$group==trim($columns[3]) ){
+                                               echo '<tr>
+                                                <td>'.$columns[0].'</td>
+                                                 <td>'.$columns[1].'</td>
+                                                 <td>'.$columns[2].'</td>
+                                                 <td>'.$Type[trim($columns[3])].'</td>    
+                                                 </tr>';
+      $sum+= (float)$columns[2];
+                                     }   
+                                     
+               }
+          }
+  
+  
 
-<?php
-include 'includes/footer.php';
+?>
+                    
+         	<tr>
+<td>  </td>
+<td>  </td>
+<td><?php echo $sum ?></td>
+<td>  </td>
+</tr>         
+            
+            
+        </table>
+    <?PHP
+include 'footer.php';
 ?>
